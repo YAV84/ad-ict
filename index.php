a3cd065c67705fe8bdf3872fb0f2b99cea7947ce
YAV_84 <yann.vonderscher+git@gmail.com>
2017-12-30 16:01:41 +0100

    basic structure + placeholder page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Welcome to ADICT</title>
+		<link rel="stylesheet" type="text/css" href="./assets/css/stylesheet.css">
+	</head>
+	<body>
+		<div id="header">
+			<img src="./assets/img/logo/Adict_NoBackground_NoMeaning.png"/>
+		</div>
+		<div id="content">
+			<div class="title">
+				<h1>Welcome to ADICT:</h1>
+				<h2>Application Development for the Internet of Connected Things</h2>
+			</div>
+			<div class="content">
+				<span>Site will be available shortly...</span>
+			</div>
+		</div>
+		<div id="footer">
+			<a href="mailto:admin@ad-ict.net">Contact us</a>
+		</div>
+		
+	</body>
+</html>
